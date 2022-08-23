9745a839f4324cd03e86d79c48255a3e96827924
jamishon <jamishon@126.com>
2022-08-23 22:50:03 +0800

    Add:ring buffer create function
    Signed-off-by: jamishon <jamishon@126.com>

--- ringbuffer/ring_buffer.cc
+++ ringbuffer/ring_buffer.cc
@@ -11,6 +11,75 @@
 
 #include "ring_buffer.h"
 
+using namespace ringbuffer;
+
+static uint32_t RingBuffer::assign_id_ = 0;
+
 RingBuffer::RingBuffer() {}
 
 RingBuffer::~RingBuffer() {}
+
+int RingBuffer::CreateRing(std::string name, uint32_t size, int flag,
+                           uint8_t unit_byte) {
+  Ring* r = NULL;
+  uint32_t request_size = sizeof(Ring) + size * unit_byte;
+  void* all = malloc(request_size);
+  if (r == NULL) return -1;
+
+  r->addr = all;
+  r->capacity = size - 1;
+  r->mask = size - 1;
+  r->size = size;
+  r->flags = flag;
+  r->name = name;
+  r->prod.head = 0;
+  r->prod.tail = 0;
+  r->prod.type = MT;
+  r->cons.head = 0;
+  r->cons.tail = 0;
+  r->prod.type = MT;
+  r->ring_id = assign_id_++;
+
+  std::lock_guard<std::mutex> guard(mtx_);
+
+  ring_buffers_.emplace(r->ring_id, r);
+
+  return r->ring_id;
+}
+
+// int InitRing(Ring* r) { return 0; }
+int RingBuffer::ResetRing(uint32_t ring_id) {
+  Ring* r = NULL;
+  {
+    std::lock_guard<std::mutex> guard(mtx_);
+
+    r = ring_buffers_.at(ring_id);
+  }
+  if (r == NULL) return -1;
+
+  r->prod.head = 0;
+  r->prod.tail = 0;
+  r->cons.head = 0;
+  r->cons.tail = 0;
+
+  return 0;
+}
+
+int RingBuffer::FreeRing(uint32_t ring_id) {
+  std::lock_guard<std::mutex> guard(mtx);
+  Ring* r = ring_buffers_.at(ring_id);
+  if (r == NULL) return -1;
+  ring_buffers_.erase(ring_id);
+}
+
+Ring* GetRing(uint32_t ring_id) {
+  std::lock_guard<std::mutex> guard(mtx);
+  Ring* r = ring_buffers_.at(ring_id);
+  if (r == NULL) return NULL;
+
+  return r;
+}
+
+int Enqueue(void* data, int num) { return 0; }
+
+int Dequeue(void* data, int num) { return 0; }
--- ringbuffer/ring_buffer.h
+++ ringbuffer/ring_buffer.h
@@ -8,9 +8,79 @@
  * @copyright Copyright (c) 2022
  *
  */
+#include <mutex>
+#include <string>
+#include <map>
+
+#define ALIGN_BYTE 4
+#define ALIGNED __attribute__((__aligned__(ALIGN_BYTE)))
+
+namespace ringbuffer {
+
+struct Entry {
+  EntryNext next;
+  void* data;
+};
+
+struct EntryNext {
+  Entry* next;
+  Entry** prev;
+};
+
+// struct RingList {
+//   Entry* first;
+//   Entry** last;
+// };
+
+enum SyncType {
+  MT,  // multi-thread access
+  ST,  // single-thread
+  MT_RTS,
+  MT_HTS
+};
+
+struct RingHeadTail {
+  volatile uint32_t head;
+  volatile uint32_t tail;
+  SyncType type;
+};
+
+struct Ring {
+  uint32_t ring_id;
+  std::string name;
+  int flags;
+  uint32_t size;
+  uint32_t mask;
+  uint32_t capacity;
+
+  void* addr;  // Allocated ring array conent address
+
+  RingHeadTail prod;
+
+  RingHeadTail cons;
+};
 
 class RingBuffer {
  public:
   RingBuffer();
   ~RingBuffer();
+
+  int CreateRing(std::string name, uint32_t size, int flag, uint8_t unit_byte = sizeof(void *));
+  //int InitRing(Ring* r);
+  int ResetRing(uint32_t ring_id);
+  int FreeRing(uint32_t ring_id);
+  //int AddRing(Ring *r);
+  int RemoveRing(uint32_t ring_id);
+  Ring* GetRing(uint32_t ring_id);
+
+  int Enqueue(uint32_t ring_id, void* data, int num);
+  int Dequeue(uint32_t ring_id, void* data, int num);
+
+ private:
+  //RingList ring_list_;
+  std::map<uint32_t, Ring *> ring_buffers_;
+  std::mutex mtx_;
+  static uint32_t assign_id_;
 };
+
+}  // namespace ringbuffer
