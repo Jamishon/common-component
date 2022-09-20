91943ccdf71cd7244c00bdd4f20838ad3165f39a
jamishon <jamishon@126.com>
2022-09-20 21:10:52 +0800

    Add redblacktree
    Signed-off-by: jamishon <jamishon@126.com>

---
+++ redblacktree/binary_tree.h
@@ -0,0 +1,35 @@
+/**
+ * @file binary_tree.cc
+ * @author Jamishon
+ * @brief Binary search tree.
+ * @version 0.1
+ * @date 2022-09-20
+ *
+ * @copyright Copyright (c) 2022
+ *
+ */
+
+struct TreeNode {
+  void* data;
+  TreeNode* left;
+  TreeNode* right;
+  TreeNode* parent;
+};
+
+class BinaryTree {
+ public:
+  int Search(TreeNode* node);
+  TreeNode* Minimum(TreeNode* root);
+  TreeNode* Maximum(TreeNode* root);
+  TreeNode* Predecessor(TreeNode* node);
+  TreeNode* Successor(TreeNode* node);
+  int Insert(TreeNode* node);
+  int Delete(TreeNode* node);
+
+  int PreorderTraverse(TreeNode* root);
+  int PostorderTraverse(TreeNode* root);
+  int InorderTraverse(TreeNode* root);
+
+ private:
+  TreeNode* root;
+};
---
+++ redblacktree/main.cc
@@ -0,0 +1,14 @@
+/**
+ * @file main.cc
+ * @author Jamishon
+ * @brief Red-black tree
+ * @version 0.1
+ * @date 2022-09-20
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ */
+
+int main() {
+    
+}
